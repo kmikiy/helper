d89e595a260528e3c4847b6b4e8031d3d2d34ae0
kmikiy <m.kristyan@gmail.com>
2019-01-13 21:01:41 +0100

    labeled package added

---
+++ labaled/labeled.go
@@ -0,0 +1,77 @@
+package labeled
+
+func Bool(label string, value bool) bool {
+	return value
+}
+
+func String(label string, value string) string {
+	return value
+}
+
+func Int8(label string, value int8) int8 {
+	return value
+}
+
+func UInt8(label string, value uint8) uint8 {
+	return value
+}
+
+func Byte(label string, value byte) byte {
+	return value
+}
+
+func Int16(label string, value int16) int16 {
+	return value
+}
+
+func UInt16(label string, value uint16) uint16 {
+	return value
+}
+
+func Int32(label string, value int32) int32 {
+	return value
+}
+
+func Rune(label string, value rune) rune {
+	return value
+}
+
+func UInt32(label string, value uint32) uint32 {
+	return value
+}
+
+func Int64(label string, value int64) int64 {
+	return value
+}
+
+func UInt64(label string, value uint64) uint64 {
+	return value
+}
+
+func Int(label string, value int) int {
+	return value
+}
+
+func UInt(label string, value uint) uint {
+	return value
+}
+
+func UIntPtr(label string, value uintptr) uintptr {
+	return value
+}
+
+func Float32(label string, value float32) float32 {
+	return value
+}
+
+func Float64(label string, value float64) float64 {
+	return value
+}
+
+func Complex64(label string, value complex64) complex64 {
+	return value
+}
+
+func Complex128(label string, value complex128) complex128 {
+	return value
+}
---
+++ labaled/labeled_test.go
@@ -0,0 +1,545 @@
+package labeled
+
+import (
+	"testing"
+)
+
+func TestBool(t *testing.T) {
+	type args struct {
+		label string
+		value bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "True",
+			args: args{
+				label: "True",
+				value: true,
+			},
+			want: true,
+		},
+		{
+			name: "False",
+			args: args{
+				label: "False",
+				value: false,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Bool(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Bool() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	type args struct {
+		label string
+		value string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "String",
+			args: args{
+				label: "Random String",
+				value: "random string",
+			},
+			want: "random string",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := String(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInt8(t *testing.T) {
+	type args struct {
+		label string
+		value int8
+	}
+	tests := []struct {
+		name string
+		args args
+		want int8
+	}{
+		{
+			name: "Int8",
+			args: args{
+				label: "Random Int8",
+				value: 8,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Int8(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Int8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUInt8(t *testing.T) {
+	type args struct {
+		label string
+		value uint8
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint8
+	}{
+		{
+			name: "UInt8",
+			args: args{
+				label: "Random UInt8",
+				value: 8,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UInt8(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UInt8() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestByte(t *testing.T) {
+	type args struct {
+		label string
+		value byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want byte
+	}{
+		{
+			name: "Byte",
+			args: args{
+				label: "s Byte",
+				value: 's',
+			},
+			want: 's',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Byte(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Byte() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInt16(t *testing.T) {
+	type args struct {
+		label string
+		value int16
+	}
+	tests := []struct {
+		name string
+		args args
+		want int16
+	}{
+		{
+			name: "Int16",
+			args: args{
+				label: "Random Int16",
+				value: 16,
+			},
+			want: 16,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Int16(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Int16() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUInt16(t *testing.T) {
+	type args struct {
+		label string
+		value uint16
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint16
+	}{
+		{
+			name: "UInt16",
+			args: args{
+				label: "Random UInt16",
+				value: 16,
+			},
+			want: 16,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UInt16(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UInt16() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInt32(t *testing.T) {
+	type args struct {
+		label string
+		value int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{
+			name: "Int32",
+			args: args{
+				label: "Random Int32",
+				value: 32,
+			},
+			want: 32,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Int32(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Int32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRune(t *testing.T) {
+	type args struct {
+		label string
+		value rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+		{
+			name: "Rune",
+			args: args{
+				label: "j Rune",
+				value: 'j',
+			},
+			want: 'j',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Rune(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Rune() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUInt32(t *testing.T) {
+	type args struct {
+		label string
+		value uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{
+			name: "UInt32",
+			args: args{
+				label: "Random UInt32",
+				value: 32,
+			},
+			want: 32,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UInt32(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UInt32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInt64(t *testing.T) {
+	type args struct {
+		label string
+		value int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Int64",
+			args: args{
+				label: "Random Int64",
+				value: 64,
+			},
+			want: 64,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Int64(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Int64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUInt64(t *testing.T) {
+	type args struct {
+		label string
+		value uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		{
+			name: "UInt64",
+			args: args{
+				label: "Random UInt64",
+				value: 64,
+			},
+			want: 64,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UInt64(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UInt64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInt(t *testing.T) {
+	type args struct {
+		label string
+		value int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Int",
+			args: args{
+				label: "Random Int",
+				value: 8,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Int(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Int() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUInt(t *testing.T) {
+	type args struct {
+		label string
+		value uint
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint
+	}{
+		{
+			name: "UInt",
+			args: args{
+				label: "Random UInt",
+				value: 8,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UInt(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUIntPtr(t *testing.T) {
+	type args struct {
+		label string
+		value uintptr
+	}
+	tests := []struct {
+		name string
+		args args
+		want uintptr
+	}{
+		{
+			name: "UIntPtr",
+			args: args{
+				label: "Random UIntPtr",
+				value: 8,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UIntPtr(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("UIntPtr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFloat32(t *testing.T) {
+	type args struct {
+		label string
+		value float32
+	}
+	tests := []struct {
+		name string
+		args args
+		want float32
+	}{
+		{
+			name: "Float32",
+			args: args{
+				label: "Random Float32",
+				value: 32.0,
+			},
+			want: 32.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Float32(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Float32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFloat64(t *testing.T) {
+	type args struct {
+		label string
+		value float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "Float64",
+			args: args{
+				label: "Random Float64",
+				value: 64.0,
+			},
+			want: 64.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Float64(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Float64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestComplex64(t *testing.T) {
+	type args struct {
+		label string
+		value complex64
+	}
+	tests := []struct {
+		name string
+		args args
+		want complex64
+	}{
+		{
+			name: "Complex64",
+			args: args{
+				label: "Random Complex64",
+				value: 64 + 0i,
+			},
+			want: 64 + 0i,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Complex64(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Complex64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestComplex128(t *testing.T) {
+	type args struct {
+		label string
+		value complex128
+	}
+	tests := []struct {
+		name string
+		args args
+		want complex128
+	}{
+		{
+			name: "Complex128",
+			args: args{
+				label: "Random Complex128",
+				value: 128 + 0i,
+			},
+			want: 128 + 0i,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Complex128(tt.args.label, tt.args.value); got != tt.want {
+				t.Errorf("Complex128() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
